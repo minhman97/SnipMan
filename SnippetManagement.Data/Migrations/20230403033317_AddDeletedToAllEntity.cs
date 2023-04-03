454fc02994e4e7e21c09f9bf253505e844422bde
Nguyen Gia Hoang <hoang.g@pattern.co.nz>
2023-04-03 11:25:18 +0700

    7 - add deleted to all entity

---
+++ SnippetManagement.Data/Migrations/20230403033317_AddDeletedToAllEntity.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SnippetManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeletedToAllEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Tag",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "SnippetTag",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Tag");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "SnippetTag");
+        }
+    }
+}
