2dccbdd4b67c1916edb851049e5166f2e18401ad
Nguyen Gia Hoang <hoang.g@pattern.co.nz>
2023-05-04 10:09:46 +0700

    10 - view line numbers

--- snippet_management.web/src/components/SnippetTextArea.js
+++ snippet_management.web/src/components/SnippetTextArea.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { updateSnippet } from "../api/SnippetApi";
 import { toast } from "react-toastify";
 import {
@@ -13,13 +13,15 @@ export const SnippetTextArea = ({ priviousSnippetContent }) => {
   const { token, snippet, setSnippet } = useSnippetContext();
   const [statusSnippetTextArea, setStatusSnippetTextArea] =
     useState("edit_status");
+  const refTextArea = useRef();
+  const refEditor = useRef();
   const getBtnControlSnippetTextArea = (status) => {
     let btn = "";
     switch (status) {
       case "exit_status":
         btn = (
           <button
-            className="p-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm absolute mt-5 mr-5 right-0 hover:bg-slate-700"
+            className="p-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm absolute right-8 top-8 hover:bg-slate-700"
             onClick={() => {
               setStatusSnippetTextArea("edit_status");
             }}
@@ -31,7 +33,7 @@ export const SnippetTextArea = ({ priviousSnippetContent }) => {
       case "save_status":
         btn = (
           <button
-            className="p-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm absolute mt-5 mr-5 right-0 hover:bg-slate-700"
+            className="p-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm absolute right-8 top-8 hover:bg-slate-700"
             onClick={async () => {
               setStatusSnippetTextArea("edit_status");
               var res = await updateSnippet(token, snippet);
@@ -69,7 +71,7 @@ export const SnippetTextArea = ({ priviousSnippetContent }) => {
       default:
         btn = (
           <button
-            className="p-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm absolute right-6 top-6 hover:bg-slate-700"
+            className="p-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm absolute right-8 top-8 hover:bg-slate-700"
             onClick={() => {
               setStatusSnippetTextArea("exit_status");
             }}
@@ -84,23 +86,46 @@ export const SnippetTextArea = ({ priviousSnippetContent }) => {
 
   return (
     <div className="relative">
-      <textarea
-        className="bg-slate-950 w-full min-h-[65vh] rounded-lg"
-        value={snippet.content}
-        {...(statusSnippetTextArea !== "exit_status" &&
-        statusSnippetTextArea !== "save_status"
-          ? { readOnly: true }
-          : {})}
-        onChange={(e) => {
-          setSnippet({ ...snippet, content: e.target.value });
-          setStatusSnippetTextArea("save_status");
-        }}
-      ></textarea>
+      <div className="h-[65vh] overflow-auto">
+        <div ref={refEditor} className="textarea-container">
+          <div className="line-numbers">
+            {snippet !== undefined &&
+              snippet.content !== undefined &&
+              snippet.content.split("\n").map((v, i) => {
+                return <span key={i}></span>;
+              })}
+          </div>
+          <textarea
+            ref={refTextArea}
+            className="bg-slate-950 rounded-lg w-full"
+            rows={28}
+            value={snippet.content}
+            {...(statusSnippetTextArea !== "exit_status" &&
+            statusSnippetTextArea !== "save_status"
+              ? { readOnly: true }
+              : {})}
+            onChange={(e) => {
+              setSnippet({ ...snippet, content: e.target.value });
+              setStatusSnippetTextArea("save_status");
+            }}
+            onKeyUp={(event) => {
+              if (event.key === "Tab") {
+                const start = event.target.selectionStart;
+                const end = event.target.selectionEnd;
+
+                event.target.value =
+                  event.target.value.substring(0, start) +
+                  "\t" +
+                  event.target.value.substring(end);
+
+                event.preventDefault();
+              }
+            }}
+          ></textarea>
+        </div>
+      </div>
       {getBtnControlSnippetTextArea(statusSnippetTextArea)}
-      <button
-        className="p-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm absolute right-6 bottom-6 hover:bg-slate-700"
-        onClick={() => {}}
-      >
+      <button className="p-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm absolute right-8 bottom-8 hover:bg-slate-700">
         <Bars3BottomLeftIcon className="h-6 w-6 text-white-500" />
       </button>
     </div>
