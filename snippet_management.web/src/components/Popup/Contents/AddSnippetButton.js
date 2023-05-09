40e9a4f5aa8d0b106ef3dac0b4cedbdbe8605690
Nguyen Gia Hoang <hoang.g@pattern.co.nz>
2023-05-09 17:39:44 +0700

    #16 - setup react query

--- snippet_management.web/src/components/Popup/Contents/AddSnippetButton.js
+++ snippet_management.web/src/components/Popup/Contents/AddSnippetButton.js
@@ -6,7 +6,7 @@ const AddSnippetButton = () => {
       <a
         href="/Snippet/Create"
         type="button"
-        className="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm"        
+        className="px-4 py-2 font-semibold text-sm bg-slate-700  text-white rounded-full shadow-sm"        
       >
         Create from scratch
       </a>
